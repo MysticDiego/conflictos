87efe47c1d94d8317c977e2efcadbf1f0a27d3dd
Diego Rodríguez <diegoissac999@gmail.com>
2026-09-08 11:41:32 -0500

    Agregando nueva clase

---
+++ Suma.java
@@ -0,0 +1,31 @@
+public class Suma {
+    private int numA, numB, numC;
+
+    public int getNumA() {
+        return numA;
+    }
+
+    public void setNumA(int numA) {
+        this.numA = numA;
+    }
+
+    public int getNumB() {
+        return numB;
+    }
+
+    public void setNumB(int numB) {
+        this.numB = numB;
+    }
+
+    public int getNumC() {
+        return numC;
+    }
+
+    public void setNumC(int numC) {
+        this.numC = numC;
+    }
+
+    public int suma() {
+        return numA + numB + numC;
+    }
+}
